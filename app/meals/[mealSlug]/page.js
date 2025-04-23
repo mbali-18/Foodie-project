6da1296ce33b9b27e38a8c25236aa451add0f8cd
mbali-18 <faithfulservent18@gmail.com>
2025-04-23 16:48:50 +0200

    wip...

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,3 +1,32 @@
-export default function MealDetailsPage() {
-  return <h1>Meal Details</h1>;
+import Image from "next/image";
+
+import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+
+export default function MealDetailsPage({ params }) {
+  const meal = getMeal(params.mealSlug);
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill />
+          <div className={classes.headerText}>
+            <h1>{meal.title}</h1>
+            <p className={classes.creator}>
+              by <a href={`mailto:${`EMAIL`}`}>NAME</a>
+            </p>
+            <p className={classes.summary}>SUMMARY</p>
+          </div>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: "...",
+          }}
+        ></p>
+      </main>
+    </>
+  );
 }
