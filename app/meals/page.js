0de54af517bd83465f0891d210b6cf284ef6748e
mbali-18 <faithfulservent18@gmail.com>
2025-04-30 11:05:11 +0200

    wrapping up

--- app/meals/page.js
+++ app/meals/page.js
@@ -5,8 +5,15 @@ import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
 
+export const metadata = {
+  title: "All Meals",
+  description: "Browse the delicious meals shared by our vibrant community.",
+};
+
 async function Meals() {
+  console.log("Fetching meals");
   const meals = await getMeals();
+
   return <MealsGrid meals={meals} />;
 }
 
@@ -19,11 +26,10 @@ export default function MealsPage() {
           <span className={classes.highlight}>by you</span>
         </h1>
         <p>
-          Choose your favorite recipe and cook it yourself. It is easy and fun !
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
         </p>
         <p className={classes.cta}>
-          <Link href="/meals/share"></Link>
-          Share Your Favorite Recipe
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
         </p>
       </header>
       <main className={classes.main}>
